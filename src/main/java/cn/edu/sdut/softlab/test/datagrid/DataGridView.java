ef462533317cff5cc62aab46c66262f2d59145d1
gaoziqiang <gaoziqianghi@163.com>
2017-08-03 08:22:53 +0800

    DataGridView.java

---
+++ src/main/java/cn/edu/sdut/softlab/test/datagrid/DataGridView.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.edu.sdut.softlab.test.datagrid;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author gaoziqiang
+ */
+
+@ManagedBean(name = "dataGridView")
+@RequestScoped
+public class DataGridView implements Serializable {
+
+    private List<Car> cars;
+
+    private Car selectedCar;
+
+    @ManagedProperty("#{carService}")
+    private CarService service;
+
+    @PostConstruct
+    public void init() {
+        cars = service.createCars(48);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setService(CarService service) {
+        this.service = service;
+    }
+
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }
+}
