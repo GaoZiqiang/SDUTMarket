1c2c6af0cdfb4cdc00f41dafd12ecd8f57af6d5b
gaoziqiang <gaoziqianghi@163.com>
2017-08-21 11:54:25 +0800

    已发布物品

--- src/main/java/cn/edu/sdut/softlab/itemcontroller/PublishedItemController.java
+++ src/main/java/cn/edu/sdut/softlab/itemcontroller/PublishedItemController.java
@@ -5,7 +5,9 @@
  */
 package cn.edu.sdut.softlab.itemcontroller;
 
+import cn.edu.sdut.softlab.model.Picture;
 import cn.edu.sdut.softlab.model.PublishedItem;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
@@ -19,22 +21,22 @@ import javax.persistence.Query;
  *
  * @author gaoziqiang
  */
-@Named("electric")
+@Named("publishedItemController")
 @RequestScoped
-public class ElectricController {
+public class PublishedItemController {
 
     EntityManagerFactory emf;
     EntityManager em;
-    List<PublishedItem> itemList;
 
-    public List<PublishedItem> getItemList() {
-        return itemList;
+    List<Picture> digitalPicList = new ArrayList<Picture>();
+
+    public List<Picture> getDigitalPicList() {
+        return digitalPicList;
     }
 
-    public void setItemList(List<PublishedItem> itemList) {
-        this.itemList = itemList;
+    public void setDigitalPicList(List<Picture> digitalPicList) {
+        this.digitalPicList = digitalPicList;
     }
-
     //PublishedItem对象
     private PublishedItem currentPublishedItem = new PublishedItem();
 
@@ -46,21 +48,119 @@ public class ElectricController {
         this.currentPublishedItem = newPublishedItem;
     }
 
+    List<PublishedItem> electricItemList = new ArrayList<PublishedItem>();
+    List<PublishedItem> digitalItemList = new ArrayList<PublishedItem>();
+    List<PublishedItem> ticketItemList = new ArrayList<PublishedItem>();
+    List<PublishedItem> bookItemList = new ArrayList<PublishedItem>();
+    List<PublishedItem> sportItemList = new ArrayList<PublishedItem>();
+
+    public List<PublishedItem> getElectricItemList() {
+        return electricItemList;
+    }
+
+    public void setElectricItemList(List<PublishedItem> electricItemList) {
+        this.electricItemList = electricItemList;
+    }
+
+    public List<PublishedItem> getDigitalItemList() {
+        return digitalItemList;
+    }
+
+    public void setDigitalItemList(List<PublishedItem> digitalItemList) {
+        this.digitalItemList = digitalItemList;
+    }
+
+    public List<PublishedItem> getTicketItemList() {
+        return ticketItemList;
+    }
+
+    public void setTicketItemList(List<PublishedItem> ticketItemList) {
+        this.ticketItemList = ticketItemList;
+    }
+
+    public List<PublishedItem> getBookItemList() {
+        return bookItemList;
+    }
+
+    public void setBookItemList(List<PublishedItem> bookItemList) {
+        this.bookItemList = bookItemList;
+    }
+
+    public List<PublishedItem> getSportItemList() {
+        return sportItemList;
+    }
+
+    public void setSportItemList(List<PublishedItem> sportItemList) {
+        this.sportItemList = sportItemList;
+    }
+
     //闲置数码物品处理
-    public String eclectricUtil() {
+    public String digitalUtil() {
+        emf = Persistence.createEntityManagerFactory("SDUTMarket");
+        em = emf.createEntityManager();
+
+        Query query = em.createQuery("select item from PublishedItem item where item.category.name = 'digital'");
+        digitalItemList = query.getResultList();
+
+        Iterator iterator = digitalItemList.iterator();
+        if (digitalItemList.size() >= 1) {
+            while (iterator.hasNext()) {
+                currentPublishedItem = (PublishedItem) iterator.next();
+                System.out.println("---currentPublishedItem.getPicturePath()---\r\n" + currentPublishedItem.getPicturePath());
+                currentPublishedItem.getPicturePath();
+//                digitalPicList = (List<Picture>) currentPublishedItem.getPictures();
+//                Iterator newIterator = digitalPicList.iterator();
+//                while (newIterator.hasNext()) {
+//                    Picture newPicture = (Picture) newIterator.next();
+//                    newPicture.getPath();
+//                    System.out.println("---newPicture.getPath()---\r\n" + newPicture.getPath());
+//                }
+            }
+        }
+        return "digital.jsf";
+    }
+
+    //电器日用物品处理
+    public String electricUtil() {
         emf = Persistence.createEntityManagerFactory("SDUTMarket");
         em = emf.createEntityManager();
 
         Query query = em.createQuery("select item from PublishedItem item where item.category.name = 'electric'");
-        itemList = query.getResultList();
-        System.out.println("---itemList的size: " + itemList.size() + "---");
-        Iterator iterator = itemList.iterator();
-        if (itemList.size() >= 1) {
-            while (iterator.hasNext()) {
-                currentPublishedItem = (PublishedItem) iterator.next();
-            }
-        }
+        electricItemList = query.getResultList();
 
-        return "publishedItem";
+        return "electric.jsf";
+    }
+
+    //图书教材物品处理
+    public String bookUtil() {
+        emf = Persistence.createEntityManagerFactory("SDUTMarket");
+        em = emf.createEntityManager();
+
+        Query query = em.createQuery("select item from PublishedItem item where item.category.name = 'Book'");
+        bookItemList = query.getResultList();
+
+        return "book.jsf";
+    }
+
+    //运动棋牌物品处理
+    public String sportUtil() {
+        emf = Persistence.createEntityManagerFactory("SDUTMarket");
+        em = emf.createEntityManager();
+
+        Query query = em.createQuery("select item from PublishedItem item where item.category.name = 'sport'");
+        sportItemList = query.getResultList();
+
+        return "sport.jsf";
+    }
+
+    //票券小物物品处理
+    public String ticketUtil() {
+        emf = Persistence.createEntityManagerFactory("SDUTMarket");
+        em = emf.createEntityManager();
+
+        Query query = em.createQuery("select item from PublishedItem item where item.category.name = 'ticket'");
+        ticketItemList = query.getResultList();
+
+        return "ticket.jsf";
     }
 }
